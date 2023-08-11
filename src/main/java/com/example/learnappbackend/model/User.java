3c027647ba68e56cdcdce6b506ef745c3b4ff79e
Tomasz Milewski <tommilewski2003@gmail.com>
2023-08-11 10:25:13 +0200

    create user

---
+++ src/main/java/com/example/learnappbackend/model/User.java
@@ -0,0 +1,81 @@
+package com.example.learnappbackend.model;
+
+import com.example.learnappbackend.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uuid;
+    private String email;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User() {
+        generateUUID();
+    }
+
+    public User(String email, String username, String password, Role role) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        generateUUID();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    private void generateUUID() {
+        uuid = String.valueOf(UUID.randomUUID());
+    }
+}
---
+++ src/main/java/com/example/learnappbackend/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.example.learnappbackend.model.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
