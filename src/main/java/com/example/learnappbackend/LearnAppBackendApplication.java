cb0eec7e4280ccc8dd175731609e04db79eeba69
Tomasz Milewski <tommilewski2003@gmail.com>
2023-08-11 10:15:45 +0200

    initial commit

---
+++ src/main/java/com/example/learnappbackend/LearnAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnappbackend/LearnAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
